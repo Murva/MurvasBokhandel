c627e91264106d00718be000b6be01f829afd804
Rikard Olsson <rikard@thryselius.se>
2016-02-02 13:54:35 +0100

    Tema tillagt

--- MurvasBokhandel/App_Start/BundleConfig.cs
+++ MurvasBokhandel/App_Start/BundleConfig.cs
@@ -25,6 +25,7 @@ namespace MurvasBokhandel
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/theme.min.css",
                       "~/Content/site.css"));
         }
     }
