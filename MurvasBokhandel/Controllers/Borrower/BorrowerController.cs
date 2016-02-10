e23b9e34920ac2f492727baba49aa058d39bd41e
Hamsterslott <johanrasmussen94@gmail.com>
2016-02-10 14:13:57 +0100

    Mockup

---
+++ MurvasBokhandel/Controllers/Borrower/BorrowerController.cs
@@ -0,0 +1,20 @@
+﻿using MurvasBokhandel.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MurvasBokhandel.Controllers.Borrower
+{
+    public class BorrowerController : Controller
+    {
+        //
+        // GET: /Borrower/
+        public ActionResult Start()
+        {
+            BorrowerMockup mockup = new BorrowerMockup();
+            return View(mockup);
+        }
+	}
+}
