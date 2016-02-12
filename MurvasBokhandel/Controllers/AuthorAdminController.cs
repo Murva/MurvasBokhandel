4fd4daf780173aa9dbc435df7afa46acb7a23615
Rikard Olsson <rikard@thryselius.se>
2016-02-12 10:58:46 +0100

    Ändrat/Lagt till mockup

--- MurvasBokhandel/Controllers/AuthorAdminController.cs
+++ MurvasBokhandel/Controllers/AuthorAdminController.cs
@@ -8,91 +8,17 @@ using MurvasBokhandel.Controllers;
 
 namespace MurvasBokhandel.Controllers
 {
-    public class AuthorResult
-    {
-        public Mockup.AUTHOR Author { get; set; }
-        public List<Mockup.BOOK> Books { get; set; }
-    }
-
     public class AuthorAdminController : Controller
     {
-        static List<Mockup.AUTHOR> Authors = new List<Mockup.AUTHOR>()
-        {
-            new Mockup.AUTHOR() {
-                Aid = 1,
-                FirstName = "J.K",
-                LastName = "Rowling",
-                BirthYear = 1961
-            },
-            new Mockup.AUTHOR() {
-                Aid = 2,
-                FirstName = "Liza",
-                LastName = "Marklund",
-                BirthYear = 1967
-            },
-            new Mockup.AUTHOR() {
-                Aid = 3,
-                FirstName = "Astrid",
-                LastName = "Lindgren",
-                BirthYear = 1917
-            }
-        };
-
-        static List<Mockup.BOOK_AUTHOR> Book_Authers = new List<Mockup.BOOK_AUTHOR>() {
-            new Mockup.BOOK_AUTHOR() { Aid = 1, ISBN = 9789129697704 },
-            new Mockup.BOOK_AUTHOR() { Aid = 1, ISBN = 9789129675559 },
-            new Mockup.BOOK_AUTHOR() { Aid = 1, ISBN = 9789129675566 },
-            new Mockup.BOOK_AUTHOR() { Aid = 2, ISBN = 9789164204530 },
-            new Mockup.BOOK_AUTHOR() { Aid = 2, ISBN = 9789175790336 },
-            new Mockup.BOOK_AUTHOR() { Aid = 3, ISBN = 9789129697308 },
-            new Mockup.BOOK_AUTHOR() { Aid = 4, ISBN = 9789129698442 },
-        };
-
-        static List<Mockup.BOOK> Books = new List<Mockup.BOOK>()
-        {
-            new Mockup.BOOK() { ISBN=9789129697704, Title="Harry Potter och De vises sten", SignId = 0, Pages = "512", PublicationYear = 1997, Publicationinfo = "No info"},
-            new Mockup.BOOK() { ISBN=9789129675559, Title="Harry Potter och hemligheternas kammare", SignId = 0, Pages = "751", PublicationYear = 1999, Publicationinfo = "No info"},
-            new Mockup.BOOK() { ISBN=9789129675566, Title="Harry Potter och fången från Azkaban", SignId = 0, Pages = "895", PublicationYear = 2001, Publicationinfo = "No info"},
-            new Mockup.BOOK() { ISBN=9789164204530, Title="Järnblod", SignId = 0, Pages = "523", PublicationYear = 2011, Publicationinfo = "No info"},
-            new Mockup.BOOK() { ISBN=9789175790336, Title="Lyckliga gatan", SignId = 0, Pages = "566", PublicationYear = 2012, Publicationinfo = "No info"},
-            new Mockup.BOOK() { ISBN=9789129697308, Title="Allrakäraste syster", SignId = 0, Pages = "876", PublicationYear = 1975, Publicationinfo = "No info"},
-            new Mockup.BOOK() { ISBN=9789129698442, Title="Känner du Pippi Långstrump?", SignId = 0, Pages = "120", PublicationYear = 1961, Publicationinfo = "No info"}
-        };
-
-        static List<AuthorResult> AuthorsWithBooksResults = new List<AuthorResult>() {
-            new AuthorResult() {
-                Author = Authors[0],
-                Books = new List<Mockup.BOOK>() {
-                    Books[0],
-                    Books[1],
-                    Books[2]
-                }
-            },
-            new AuthorResult() {
-                Author = Authors[1],
-                Books = new List<Mockup.BOOK>() {
-                    Books[3],
-                    Books[4]
-                }
-            },
-            new AuthorResult() {
-                Author = Authors[2],
-                Books = new List<Mockup.BOOK>() {
-                    Books[5],
-                    Books[6]
-                } 
-            }
-        };
-
         // GET: AuthorAdmin
         public ActionResult Start(string orderBy = "id")
         {
             if (orderBy == "id")
-                return View(Authors.OrderBy(author => author.Aid).ToList());
+                return View(Mockup.Authors.OrderBy(author => author.Aid).ToList());
             else if (orderBy == "name")
-                return View(Authors.OrderBy(author => author.FirstName).ToList());
+                return View(Mockup.Authors.OrderBy(author => author.FirstName).ToList());
             else
-                return View(Authors.OrderBy(author => author.BirthYear).ToList());
+                return View(Mockup.Authors.OrderBy(author => author.BirthYear).ToList());
         }
 
         public ActionResult Author(int id)
@@ -100,7 +26,7 @@ namespace MurvasBokhandel.Controllers
             if (id == 0)
                 return RedirectToAction("Start");
 
-            AuthorResult a = (AuthorResult)AuthorsWithBooksResults.Where(author => author.Author.Aid == id).First();
+            AuthorWithBooks a = (AuthorWithBooks)Mockup.AuthorsWithBooksResults.Where(author => author.Author.Aid == id).First();
 
             if (TempData.Count != 0)
             {
@@ -113,9 +39,9 @@ namespace MurvasBokhandel.Controllers
             return View(a);
         }
 
-        public ActionResult Update(AuthorResult a)
+        public ActionResult Update(AuthorWithBooks a)
         {
-            AuthorResult ar = (AuthorResult)AuthorsWithBooksResults.Where(author => author.Author.Aid == a.Author.Aid).First();
+            AuthorWithBooks ar = (AuthorWithBooks) Mockup.AuthorsWithBooksResults.Where(author => author.Author.Aid == a.Author.Aid).First();
             ar.Author.FirstName = a.Author.FirstName;
             ar.Author.LastName = a.Author.LastName;
 
@@ -132,21 +58,21 @@ namespace MurvasBokhandel.Controllers
 
         public ActionResult Store(Mockup.AUTHOR a)
         {
-            a.Aid = AuthorsWithBooksResults.OrderByDescending(author => author.Author.Aid).First().Author.Aid + 1;
-            AuthorResult ar = new AuthorResult()
+            a.Aid = Mockup.AuthorsWithBooksResults.OrderByDescending(author => author.Author.Aid).First().Author.Aid + 1;
+            AuthorWithBooks ar = new AuthorWithBooks()
             {
                 Author = a,
                 Books = new List<Mockup.BOOK>()
             };
-            Authors.Add(a);
-            AuthorsWithBooksResults.Add(ar);
+            Mockup.Authors.Add(a);
+            Mockup.AuthorsWithBooksResults.Add(ar);
 
             return RedirectToAction("Start");
         }
 
-        public ActionResult Remove(AuthorResult a)
+        public ActionResult Remove(AuthorWithBooks a)
         {
-            AuthorsWithBooksResults.Remove(AuthorsWithBooksResults.Where(author => author.Author.Aid == a.Author.Aid).ElementAt(0));
+            Mockup.AuthorsWithBooksResults.Remove(Mockup.AuthorsWithBooksResults.Where(author => author.Author.Aid == a.Author.Aid).ElementAt(0));
 
             /*
              foreach author.books, remove
@@ -167,7 +93,7 @@ namespace MurvasBokhandel.Controllers
                 SignId = 0
             };
 
-            AuthorsWithBooksResults.Where(author => author.Author.Aid == Aid).First().Books.Add(book);
+            Mockup.AuthorsWithBooksResults.Where(author => author.Author.Aid == Aid).First().Books.Add(book);
 
             return Redirect("Author/"+Aid);
         }
--- MurvasBokhandel/Models/Mockup.cs
+++ MurvasBokhandel/Models/Mockup.cs
@@ -33,30 +33,110 @@ namespace MurvasBokhandel.Models
             public string Library { get; set; }
         }
         public class STATUS {
-            public int statusid;
-            public string status;
+            public int statusid { get; set; }
+            public string status { get; set; }
         }
         public class CLASSIFICATION {
-            public int SignId;
-            public string Signum, Description;
+            public int SignId { get; set; }
+            public string Signum { get; set; }
+            public string Description { get; set; }
         }
         public class BORROW {
-            public int Barcode, PersonId;
-            public DateTime BorrowDate, ToBeReturnedDate,
-                            ReturnDate;
+            public long Barcode { get; set; }
+            public long PersonId { get; set; }
+            public DateTime BorrowDate { get; set; }
+            public DateTime ToBeReturnedDate { get; set; }
+            public DateTime ReturnDate { get; set; }
         }
         public class BORROWER {
-            public int PersonId, CategoryId;
-            public string FirstName, LastName,
-                          Address, Telno;
+            public long PersonId { get; set; }
+            public int CategoryId { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string Address { get; set; } 
+            public string Telno { get; set; }
         }
         public class CATEGORY {
             public int CategoryId, Penaltyperday;
             public string Category, Period;
         }
 
-        // Skapa egna klasser här //
-        
+        //* ------------------------------------------------ LISTOR ---------------------------------------------------  *//
+
+        public static List<Mockup.AUTHOR> Authors = new List<Mockup.AUTHOR>()
+        {
+            new Mockup.AUTHOR() {
+                Aid = 1,
+                FirstName = "J.K",
+                LastName = "Rowling",
+                BirthYear = 1961
+            },
+            new Mockup.AUTHOR() {
+                Aid = 2,
+                FirstName = "Liza",
+                LastName = "Marklund",
+                BirthYear = 1967
+            },
+            new Mockup.AUTHOR() {
+                Aid = 3,
+                FirstName = "Astrid",
+                LastName = "Lindgren",
+                BirthYear = 1917
+            }
+        };
+
+       public static List<Mockup.BOOK_AUTHOR> Book_Authers = new List<Mockup.BOOK_AUTHOR>() {
+            new Mockup.BOOK_AUTHOR() { Aid = 1, ISBN = 9789129697704 },
+            new Mockup.BOOK_AUTHOR() { Aid = 1, ISBN = 9789129675559 },
+            new Mockup.BOOK_AUTHOR() { Aid = 1, ISBN = 9789129675566 },
+            new Mockup.BOOK_AUTHOR() { Aid = 2, ISBN = 9789164204530 },
+            new Mockup.BOOK_AUTHOR() { Aid = 2, ISBN = 9789175790336 },
+            new Mockup.BOOK_AUTHOR() { Aid = 3, ISBN = 9789129697308 },
+            new Mockup.BOOK_AUTHOR() { Aid = 4, ISBN = 9789129698442 },
+        };
+
+       public static List<Mockup.BOOK> Books = new List<Mockup.BOOK>()
+        {
+            new Mockup.BOOK() { ISBN=9789129697704, Title="Harry Potter och De vises sten", SignId = 0, Pages = "512", PublicationYear = 1997, Publicationinfo = "No info"},
+            new Mockup.BOOK() { ISBN=9789129675559, Title="Harry Potter och hemligheternas kammare", SignId = 0, Pages = "751", PublicationYear = 1999, Publicationinfo = "No info"},
+            new Mockup.BOOK() { ISBN=9789129675566, Title="Harry Potter och fången från Azkaban", SignId = 0, Pages = "895", PublicationYear = 2001, Publicationinfo = "No info"},
+            new Mockup.BOOK() { ISBN=9789164204530, Title="Järnblod", SignId = 0, Pages = "523", PublicationYear = 2011, Publicationinfo = "No info"},
+            new Mockup.BOOK() { ISBN=9789175790336, Title="Lyckliga gatan", SignId = 0, Pages = "566", PublicationYear = 2012, Publicationinfo = "No info"},
+            new Mockup.BOOK() { ISBN=9789129697308, Title="Allrakäraste syster", SignId = 0, Pages = "876", PublicationYear = 1975, Publicationinfo = "No info"},
+            new Mockup.BOOK() { ISBN=9789129698442, Title="Känner du Pippi Långstrump?", SignId = 0, Pages = "120", PublicationYear = 1961, Publicationinfo = "No info"}
+        };
+
+       //* ------------------------------------------------ LISTOR END ---------------------------------------------------  *//
+
+       //* ------------------------------------------------ RESULTATLISTOR ---------------------------------------------------  *//
+
+       public static List<AuthorWithBooks> AuthorsWithBooksResults = new List<AuthorWithBooks>() {
+            new AuthorWithBooks() {
+                Author = Mockup.Authors[0],
+                Books = new List<Mockup.BOOK>() {
+                    Mockup.Books[0],
+                    Mockup.Books[1],
+                    Mockup.Books[2]
+                }
+            },
+            new AuthorWithBooks() {
+                Author = Mockup.Authors[1],
+                Books = new List<Mockup.BOOK>() {
+                    Mockup.Books[3],
+                    Mockup.Books[4]
+                }
+            },
+            new AuthorWithBooks() {
+                Author = Mockup.Authors[2],
+                Books = new List<Mockup.BOOK>() {
+                    Mockup.Books[5],
+                    Mockup.Books[6]
+                } 
+            }
+        };
+
+       //* ------------------------------------------------ RESULTATLISTOR END ---------------------------------------------------  *//
+
         public Mockup() { 
                         
         }
