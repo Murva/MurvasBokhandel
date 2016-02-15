c3ffe86a6f583ebf9cff91da545e857bbbd872cf
denilsson2 <denilsson2@gmail.com>
2016-02-15 10:55:24 +0100

    Sarch klar med ny mockup

--- MurvasBokhandel/Controllers/PublicController.cs
+++ MurvasBokhandel/Controllers/PublicController.cs
@@ -19,33 +19,7 @@ namespace MurvasBokhandel.Controllers
 
     public class PublicController : Controller
     {
-        //List<BookResult> SearchResults = new List<BookResult>() 
-        //    {
-        //        new BookResult() 
-        //        {
-        //            Aid = 1,
-        //            ISBN = 11111,
-        //            Title = "Harry Potter och fången från Azkaban",
-        //            FirstName = "J.K",
-        //            LastName = "Rowling"
-        //        },
-        //        new BookResult() 
-        //        {
-        //            Aid = 1,
-        //            ISBN = 22222,
-        //            Title = "Harry Pulver och Kalle",
-        //            FirstName = "J.K",
-        //            LastName = "Rowling"
-        //        },
-        //        new BookResult() 
-        //        {
-        //            Aid = 2,
-        //            ISBN = 33333333,
-        //            Title = "En murvig Murva",
-        //            FirstName = "Murvan",
-        //            LastName = "Murvansson"
-        //        }
-        //    };
+       
         // GET: Public
         public ActionResult Start()
         {
@@ -53,10 +27,7 @@ namespace MurvasBokhandel.Controllers
             return View();
         }
 
-        //public ActionResult Book(int id)
-        //{
-        //    return View();
-        //}
+       
 
         [HttpGet]
         public ActionResult Search(string search_field)
