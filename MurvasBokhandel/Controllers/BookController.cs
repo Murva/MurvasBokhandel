14bea4f2dc2868820844dee897ba5c398d5ce37a
Hamsterslott <johanrasmussen94@gmail.com>
2016-03-01 13:36:28 +0100

    Author utskrift update

--- MurvasBokhandel/Controllers/BookController.cs
+++ MurvasBokhandel/Controllers/BookController.cs
@@ -8,16 +8,13 @@ using MurvasBokhandel.Controllers;
 
 namespace MurvasBokhandel.Controllers
 {
-    
     public class BookController : Controller
     {
         
-        //};
         // GET: Book
         public ActionResult GetBook(long isbn)
         {
-            BookWithAuthor bwa = (BookWithAuthor)Mockup.BooksWithAuthorResult.Where(book => book.Book.ISBN == isbn).First();
-            
+            BookWithAuthor bwa = (BookWithAuthor)Mockup.BooksWithAuthorResult.Where(book => book.Book.ISBN == isbn).First();           
             return View(bwa);
         }
     }
