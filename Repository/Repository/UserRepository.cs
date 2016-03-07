c71b0d3c955c86b7a2c5051b75a5bf1ffc3c02d3
Rikard Olsson <rikard@thryselius.se>
2016-03-07 13:56:23 +0100

    Skapat alla repository och entitymodels till login

--- Repository/Repository/UserRepository.cs
+++ Repository/Repository/UserRepository.cs
@@ -9,5 +9,20 @@ namespace Repository.Repository
 {
     public class UserRepository : BaseRepository
     {
+        public static user MapAuthor(SqlDataReader dar)
+        {
+            author authObj = new author();
+            authObj.Aid = Convert.ToInt32(dar["Aid"]);
+            authObj.FirstName = dar["FirstName"] as string;
+            authObj.LastName = dar["LastName"] as string;
+            authObj.BirthYear = dar["BirthYear"] as string;
+
+            return authObj;
+        }
+
+        public bool dbUserExists(string email)
+        {
+
+        }
     }
 }
