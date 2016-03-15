fcb6d7d19b30284ceca0c79599b3f4a1a5386461
Hamsterslott <johanrasmussen94@gmail.com>
2016-03-15 16:34:29 +0100

    Placeholder text borrower

--- Repository/EntityModel/borrower.cs
+++ Repository/EntityModel/borrower.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,11 +9,17 @@ namespace Repository.EntityModel
 {
     public class borrower
     {
+        [Required(ErrorMessage = "Du måste fylla i Personnummer")]
         public string PersonId { get; set; }
+        [Required(ErrorMessage = "Du måste fylla i Kategori")]
         public int CategoryId { get; set; }
+        [Required(ErrorMessage = "Du måste fylla i Förnamn")]
         public string FirstName { get; set; }
+        [Required(ErrorMessage = "Du måste fylla i Efternamn")]
         public string LastName { get; set; }
+        [Required(ErrorMessage = "Du måste fylla i Address")]
         public string Address { get; set; }
+        [Required(ErrorMessage = "Du måste fylla i Tel.nr")]
         public string Telno { get; set; }
     }
 }
